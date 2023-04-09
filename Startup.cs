ed37bb26b752299770cf76dcd4801cb83df98c6f
Jonathan Feitor <jonathan.feitor@hotmail.com>
2023-04-08 21:57:14 -0300

    Meu primeiro envio

---
+++ Startup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CasaDoCodigo.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CasaDoCodigo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            //Serviço de seção
+            services.AddDistributedMemoryCache();
+            services.AddSession();
+
+
+            string connectionString = Configuration.GetConnectionString("Default");
+
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(connectionString)
+            );
+
+            services.AddTransient<IDataService, DataService>();
+            services.AddTransient<IProdutoRepository, ProdutoRepository>();
+            services.AddTransient<IPedidoRepository, PedidoRepository>();
+            services.AddTransient<IItemPedidoRepository, ItemPedidoRepository>();
+            services.AddTransient<ICadastroRepository, CadastroRepository>();
+
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            //Serviço de seção
+            app.UseSession();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Pedido}/{action=Carrossel}/{codigo?}");
+            });
+
+            //Garantir que o banco de dados esteja criado quando subir a aplicação
+            serviceProvider.GetService<IDataService>().InicializaDB();
+                        
+        }
+
+    }
+}
---
+++ wwwroot/js/carrinho.js
@@ -0,0 +1,71 @@
+﻿class Carrinho {
+
+    clickIncremento(btn) {
+        let data = this.getData(btn);
+        data.Quantidade++;
+        this.postQuantidade(data);
+    }
+
+    clickDecremento(btn) {
+        let data = this.getData(btn);
+        data.Quantidade--;
+        this.postQuantidade(data);
+    }
+
+    updateQuantidade(input) {
+        let data = this.getData(input);
+        this.postQuantidade(data);
+    }
+
+    getData(elemento) {
+        var linhaDoItem = $(elemento).parents('[item-id]');
+        var itemId = $(linhaDoItem).attr('item-id');
+        var novaQtde = $(linhaDoItem).find('input').val();
+        return {
+            Id: itemId,
+            Quantidade: novaQtde
+        };
+    }
+
+    postQuantidade(data) {
+        //Aplicando elemento de segurança com requisição Ajax
+        let token = $('[name=__RequestVerificationToken]').val();
+
+        let headers = {};
+        headers['RequestVerificationToken'] = token;
+        //-----**-----
+ 
+        $.ajax({
+            url: '/pedido/updatequantidade',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(data),
+            headers: headers
+        }).done(function (response) {
+            //debugger;
+
+            let itemPedido = response.itemPedido;
+            let linhaDoItem = $('[item-id=' + itemPedido.id + ']');
+
+            linhaDoItem.find('input').val(itemPedido.quantidade);
+            //linhaDoItem.find('subTotal').html((itemPedido.subTotal).duasCasas());
+            //linhaDoItem.find("[subtotal]").html((itemPedido.subtotal).duasCasas());
+
+            let carrinhoViewModel = response.carrinhoViewModel;
+
+            $('[numero-itens]').html('Total: ' + carrinhoViewModel.itens.length + ' itens');
+            $('[total]').html((carrinhoViewModel.total).duasCasas());
+
+            if (itemPedido.quantidade == 0) {
+                linhaDoItem.remove();
+            }
+        });
+    }
+}
+
+var carrinho = new Carrinho();
+
+//Função para obter 2 casas decimais e vírgula no lugar do ponto
+Number.prototype.duasCasas = function () {
+    return this.toFixed(2).replace('.' , ',');
+}
